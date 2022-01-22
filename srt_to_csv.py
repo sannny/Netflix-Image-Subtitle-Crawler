fd51516b1b33011cfdd2698d7cef2539f0595ef3
Sannidhya_work <sannidhyamalpani95@gmail.com>
2022-01-22 12:32:51 +0530

    Added srt to csv file

---
+++ srt_to_csv.py
@@ -0,0 +1,19 @@
+import pysrt
+
+import pandas as pd
+
+path = 'D:\\linux\\scratches\\Netflix_image_subtitle_crawler\\pilot_ep_1.srt'
+
+subs = pysrt.open(path)
+
+captions = [['startTime','endTime','en_text']]
+
+for sub in subs:
+    cap = [sub.start.to_time(),sub.end.to_time(),sub.text]
+    captions.append(cap)
+
+
+
+print(len(subs))
+
+print(captions)
